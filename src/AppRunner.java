355366f698af43547ff5c2740c935c1fceb6cff5
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-31 02:44:59 +0200

    feat: add menuManager.runMainMenu();

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -1,19 +1,9 @@
-import Visual.Emoji;
-import Visual.Message;
-import Visual.Separator;
+import appMenu.MenuManager;
 
 public class AppRunner {
 
   public static void main(String[] args) {
-    System.out.println(Emoji.CHEERS.getEmoji());
-    System.out.println(Message.GREETING.getMessage());
-    System.out.println(Separator.UPPER_LINE.getSeparator());
-    System.out.println("\u001B[33m•*´¨`*•.¸¸..•*´¨`*HABLAMOS ESPAÑOL•.¸¸..•*´¨`*•.¸¸.•*\u001B[0m");
-    System.out.println(Separator.DOWN_LINE.getSeparator());
-    System.out.println(Emoji.GREAT.getEmoji());
-    System.out.println(Emoji.DONE.getEmoji());
-    System.out.println(Emoji.WRONG.getEmoji());
-    System.out.println(Emoji.GO_AHEAD.getEmoji());
-    System.out.println(Emoji.WRITE_DOWN.getEmoji());
+    MenuManager menuManager = new MenuManager();
+    menuManager.runMainMenu();
   }
 }
