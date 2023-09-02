67555f8d3affae6c6e099d7b527ea203c7020756
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-02 17:22:01 +0200

    fix: add loadFromFile() and getRandomFact() methods

--- src/app/InterestingFacts.java
+++ src/app/InterestingFacts.java
@@ -1,5 +1,43 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import visual.Colors;
+import visual.Emoji;
+
 public class InterestingFacts {
 
+  private List<String> facts;
+
+  public InterestingFacts(String filePath) {
+    facts = loadFactsFromFile(filePath);
+  }
+
+  private List<String> loadFactsFromFile(String filePath) {
+    List<String> loadedFacts = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        loadedFacts.add(line);
+      }
+    } catch (IOException e) {
+      System.out.println(Colors.RED.getColor() + "Ошибка при загрузке фактов: " + e.getMessage() + Colors.RESET.getColor());
+    }
+    return loadedFacts;
+  }
+
+  public String getRandomFact() {
+    if (facts.isEmpty()) {
+      return Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Факты не найдены!" + Colors.RESET.getColor();
+    }
+    Random random = new Random();
+    int randomIndex = random.nextInt(facts.size());
+    String randomFact = facts.remove(randomIndex);
+    return randomFact;
+  }
 }
+
