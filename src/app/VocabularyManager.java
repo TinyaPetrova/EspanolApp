7ec50ec2eb92b5fa6a495319724c343ad6fd32ed
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-04 14:21:58 +0200

    feat: add methods to save and load info to/from file MyScore.txt

--- src/app/VocabularyManager.java
+++ src/app/VocabularyManager.java
@@ -1,5 +1,7 @@
 package app;
 
+import static texts.Message.SCORE_PREFIX;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Scanner;
 import visual.Colors;
 import visual.Emoji;
@@ -20,10 +23,16 @@ public class VocabularyManager {
 
   private Map<String, String> vocabulary;
   private final String filePath = "res/MyVocabulary.txt";
+  private int score = 0;
 
   public VocabularyManager() {
     vocabulary = new HashMap<>();
     loadFromFile();
+  }
+
+  public enum WordType {
+    RUSSIAN,
+    SPANISH
   }
 
   public void addWord(String russian, String spanish) {
@@ -50,7 +59,6 @@ public class VocabularyManager {
   public void saveToFileByRussian() {
     List<Entry<String, String>> sortedVocabulary = new ArrayList<>(vocabulary.entrySet());
     sortedVocabulary.sort(VocabularyComparators.byRussian());
-
     try (FileWriter writer = new FileWriter(filePath)) {
       for (Map.Entry<String, String> entry : sortedVocabulary) {
         writer.write(entry.getKey() + " - " + entry.getValue() + "\n");
@@ -64,7 +72,6 @@ public class VocabularyManager {
   public void saveToFileBySpanish() {
     List<Map.Entry<String, String>> sortedVocabulary = new ArrayList<>(vocabulary.entrySet());
     sortedVocabulary.sort(VocabularyComparators.bySpanish());
-
     try (FileWriter writer = new FileWriter(filePath)) {
       for (Map.Entry<String, String> entry : sortedVocabulary) {
         writer.write(entry.getKey() + " - " + entry.getValue() + "\n");
@@ -87,30 +94,29 @@ public class VocabularyManager {
 
   public void removeWordFromVocabulary(Scanner scanner) {
     scanner.nextLine();
-    System.out.print("Введи слово на русском, которое хочешь удалить: ");
+    System.out.print("Введи слово на русском, пару с которым хочешь удалить: ");
     String russianWord = scanner.nextLine();
     if (vocabulary.containsKey(russianWord)) {
       removeWord(russianWord);
       saveToFileByRussian();
       System.out.println(
-          Colors.PURPLE.getColor() + "Слово удалено из словаря!" + Colors.RESET.getColor());
+          Colors.PURPLE.getColor() + "Удалено из словаря!" + Colors.RESET.getColor());
     } else {
       System.out.println(Colors.RED.getColor() + "Такого слова нет!" + Colors.RESET.getColor());
     }
   }
 
-
   public void replaceWordInVocabulary(Scanner scanner) {
-    System.out.print("Введи слово на русском, которое хочешь заменить: ");
+    System.out.print("Введи слово на русском, перевод которого хочешь заменить: ");
     scanner.nextLine();
     String russianWord = scanner.nextLine();
     if (!vocabulary.containsKey(russianWord)) {
-      System.out.println("Слово не найдено в словаре.");
+      System.out.println(Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Слово не найдено в словаре" + Colors.RESET.getColor());
       return;
     }
     System.out.print("Введи новое испанское слово: ");
     String newSpanishWord = scanner.nextLine();
-    vocabulary.put(russianWord, newSpanishWord);
+    replaceWord(russianWord, newSpanishWord);
     saveToFileByRussian();
     System.out.println(
         Colors.PURPLE.getColor() + "Слово заменено в словаре!" + Colors.RESET.getColor());
@@ -141,6 +147,89 @@ public class VocabularyManager {
       }
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public String getRandomWord(WordType wordType) {
+    if (vocabulary.isEmpty()) {
+      System.out.println(Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " В словаре нет слов!" + Colors.RESET.getColor());
+      return null;
+    }
+    List<String> words = wordType.equals(WordType.RUSSIAN) ? new ArrayList<>(vocabulary.keySet()) : new ArrayList<>(vocabulary.values());
+    Random random = new Random();
+    int randomIndex = random.nextInt(words.size());
+    return words.get(randomIndex);
+  }
+
+  public String getTranslation(String word, WordType wordType) {
+    if (wordType.equals(WordType.RUSSIAN)) {
+      return vocabulary.get(word);
+    } else if (wordType.equals(WordType.SPANISH) && vocabulary.containsValue(word)) {
+      return getKeyByValue(vocabulary, word);
+    }
+    return "Перевод не найден";
+  }
+
+  private String getKeyByValue(Map<String, String> map, String value) {
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      if (entry.getValue().equals(value)) {
+        return entry.getKey();
+      }
+    }
+    return "Перевод не найден";
+  }
+
+  private int getScore() {
+    try {
+      List<String> lines = Files.readAllLines(Paths.get("res/MyScore.txt"));
+      if (!lines.isEmpty()) {
+        return Integer.parseInt(lines.get(0).trim());
+      }
+    } catch (IOException e) {
+      System.out.println(Colors.RED.getColor() + "Ошибка при чтении файла баллов: " + e.getMessage() + Colors.RESET.getColor());
+    }
+    return 0;
+  }
+
+  private void saveScore(int score) {
+    try (FileWriter writer = new FileWriter("res/MyScore.txt")) {
+      writer.write(Integer.toString(score));
+      System.out.println(Emoji.GO_AHEAD.getEmoji() + Colors.PURPLE.getColor() + " Сохранили баллы: " + score + Colors.RESET.getColor());
+    } catch (IOException e) {
+      System.out.println(Colors.RED.getColor() + "Ошибка при записи баллов в файл: " + e.getMessage() + Colors.RESET.getColor());
+    }
+  }
+
+  void incrementScore() {
+    score++;
+    saveScore(score);
+    try (FileWriter writer = new FileWriter("res/MyScore.txt")) {
+      writer.write(SCORE_PREFIX.getMessage());
+      writer.write(Integer.toString(score));
+    } catch (IOException e) {
+      System.out.println(Colors.RED.getColor() + "Ошибка при записи баллов в файл: " + e.getMessage() + Colors.RESET.getColor());
+    }
+  }
+
+  public void loadScoreFromFile() {
+
+    try {
+      List<String> lines = Files.readAllLines(Paths.get("res/MyScore.txt"));
+      if (!lines.isEmpty()) {
+        String scoreLine = lines.get(0);
+        String[] parts = scoreLine.split(":");
+        if (parts.length == 2) {
+          try {
+            score = Integer.parseInt(parts[1].trim());
+          } catch (NumberFormatException e) {
+            System.out.println(Colors.RED.getColor() + "Ошибка при загрузке баллов из файла: неверный формат данных" + Colors.RESET.getColor());
+          }
+        }
+      }
+      ProcessBuilder pb = new ProcessBuilder("notepad.exe", "res/MyScore.txt");
+      pb.start();
+    } catch (IOException e) {
+      System.out.println(Colors.RED.getColor() + "Ошибка при загрузке баллов из файла: " + e.getMessage() + Colors.RESET.getColor());
     }
   }
 }
