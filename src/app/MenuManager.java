5bf2cd5ca17dace059738adee25d7996a8a498f4
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-14 17:16:38 +0200

    fix: fix menu structure

--- src/app/MenuManager.java
+++ src/app/MenuManager.java
@@ -32,54 +32,60 @@ public class MenuManager {
         Colors.YELLOW.getColor() + Separator.DOWN_LINE.getSeparator() + Colors.RESET.getColor());
 
     while (true) {
-      System.out.println(Emoji.ONE.getEmoji() + " " + MenuText.QUIZ.getMenuText());
-      System.out.println(Emoji.TWO.getEmoji() + " " + MenuText.VOCABULARY.getMenuText());
+      System.out.println(Emoji.ONE.getEmoji() + " " + MenuText.VOCABULARY.getMenuText());
       System.out.println(
-          Emoji.THREE.getEmoji() + " " + MenuText.VOCAB_TRAININGS.getMenuText());
-      System.out.println(Emoji.FOUR.getEmoji() + " " + MenuText.ACHIEVEMENTS.getMenuText());
-      System.out.println(Emoji.FIVE.getEmoji() + " " + MenuText.RANDOM_FACTS.getMenuText());
-      System.out.println(Emoji.SIX.getEmoji() + " " + MenuText.BYE.getMenuText());
+          Emoji.TWO.getEmoji() + " " + MenuText.VOCAB_TRAININGS.getMenuText());
+      System.out.println(Emoji.THREE.getEmoji() + " " + MenuText.ACHIEVEMENTS.getMenuText());
+      System.out.println(Emoji.FOUR.getEmoji() + " " + MenuText.RANDOM_FACTS.getMenuText());
+      System.out.println(Emoji.FIVE.getEmoji() + " " + MenuText.BYE.getMenuText());
       System.out.println(
           Colors.YELLOW.getColor() + MenuText.CHOICE.getMenuText() + Colors.RESET.getColor());
 
-      int choice = scanner.nextInt();
-      switch (choice) {
-        case 1:
-          // тесты
-          break;
-        case 2:
-          // словарик
-          runVocabularySubMenu();
-          break;
-        case 3:
-          // словарные тренировки
-          runVocabularyTrainingSubMenu();
-          break;
-        case 4:
-          // достижения
-          vocabularyManager.loadScoreFromFile();
-          break;
-        case 5:
-          // факты об Испании
-          runRandomFactsSubMenu();
-          break;
-        case 6:
-          System.exit(0);
-          System.out.println(Emoji.CHEERS.getEmoji() + Colors.YELLOW.getColor() + MenuText.BYE.getMenuText() + Colors.RESET.getColor());
-        default:
+      if (scanner.hasNextInt()) {
+        int choice = scanner.nextInt();
+        if (choice >= 1 && choice <= 5) {
+          switch (choice) {
+            case 1:
+              // словарик
+              runVocabularySubMenu();
+              break;
+            case 2:
+              // словарные тренировки
+              runVocabularyTrainingSubMenu();
+              break;
+            case 3:
+              // достижения
+              vocabularyManager.loadScoreFromFile();
+              break;
+            case 4:
+              // факты об Испании
+              runRandomFactsSubMenu();
+              break;
+            case 5:
+              System.out.println(
+                  Emoji.CHEERS.getEmoji() + Colors.YELLOW.getColor() + " " + MenuText.BYE.getMenuText()
+                      + Colors.RESET.getColor());
+              System.exit(0);
+          }
+        } else {
           System.out.println(
-              Colors.RED.getColor() + Message.ERROR.getMessage() + Colors.RESET.getColor());
-          break;
+              Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Можно выбирать только среди пунктов 1-5!"
+                  + Colors.RESET.getColor());
+        }
+      } else {
+        System.out.println(
+            Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Введи цифру от 1 до 5"
+                + Colors.RESET.getColor());
+        scanner.next();
       }
     }
   }
 
   private void runVocabularySubMenu() {
-    Scanner scanner = new Scanner(System.in);
     System.out.println(
         Colors.YELLOW.getColor() + Separator.UPPER_LINE.getSeparator() + Colors.RESET.getColor());
     System.out.println(
-        Emoji.WRITE_DOWN.getEmoji() + "Здесь хранятся записанные тобой слова с переводом!");
+        Emoji.WRITE_DOWN.getEmoji() + "Здесь хранятся все записанные слова с переводом!");
     System.out.println(
         Colors.YELLOW.getColor() + Separator.DOWN_LINE.getSeparator() + Colors.RESET.getColor());
 
@@ -94,51 +100,58 @@ public class MenuManager {
       System.out.println(Emoji.EIGHT.getEmoji() + " " + MenuText.BACK_TO_MENU.getMenuText());
       System.out.println(
           Colors.YELLOW.getColor() + MenuText.CHOICE.getMenuText() + Colors.RESET.getColor());
-      int choice = scanner.nextInt();
-      switch (choice) {
-        case 1:
-          vocabularyManager.addWordToVocabulary(
-              scanner);
-          break;
-        case 2:
-          vocabularyManager.removeWordFromVocabulary(
-              scanner);
-          break;
-        case 3:
-          vocabularyManager.replaceWordInVocabulary(
-              scanner);
-          break;
-        case 4:
-          vocabularyManager.printVocabulary();
-          break;
-        case 5:
-          vocabularyManager.saveToFileByRussian();
-          break;
-        case 6:
-          vocabularyManager.saveToFileBySpanish();
-          break;
-        case 7:
-          vocabularyManager.openVocabulary();
-          break;
-        case 8:
-          System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
-              + Colors.RESET.getColor());
-          return;
-        default:
-          System.out.println(
-              Emoji.WRONG.getEmoji() + Colors.RED.getColor() + Message.ERROR.getMessage()
-                  + Colors.RESET.getColor());
-          break;
+
+      if (scanner.hasNextInt()) {
+        int choice = scanner.nextInt();
+        switch (choice) {
+          case 1:
+            vocabularyManager.addWordToVocabulary(
+                scanner);
+            break;
+          case 2:
+            vocabularyManager.removeWordFromVocabulary(
+                scanner);
+            break;
+          case 3:
+            vocabularyManager.replaceWordInVocabulary(
+                scanner);
+            break;
+          case 4:
+            vocabularyManager.printVocabulary();
+            break;
+          case 5:
+            vocabularyManager.saveToFileByRussian();
+            break;
+          case 6:
+            vocabularyManager.saveToFileBySpanish();
+            break;
+          case 7:
+            vocabularyManager.openVocabulary();
+            break;
+          case 8:
+            System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
+                + Colors.RESET.getColor());
+            return;
+          default:
+            System.out.println(
+                Emoji.WRONG.getEmoji() + Colors.RED.getColor() + Message.ERROR.getMessage()
+                    + Colors.RESET.getColor());
+            break;
+        }
+      } else {
+        System.out.println(
+            Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Введи цифру от 1 до 8"
+                + Colors.RESET.getColor());
+        scanner.next();
       }
     }
   }
 
   private void runRandomFactsSubMenu() {
-    scanner = new Scanner(System.in);
     System.out.println(
         Colors.YELLOW.getColor() + Separator.UPPER_LINE.getSeparator() + Colors.RESET.getColor());
     System.out.println(
-        Emoji.IDEA.getEmoji() + " Держи любопытный факт об Испании - у нас их много!");
+        Emoji.IDEA.getEmoji() + " Держи любопытный факт об Испании - у нас их ещё много!");
     System.out.println(
         Colors.YELLOW.getColor() + Separator.DOWN_LINE.getSeparator() + Colors.RESET.getColor());
 
@@ -147,33 +160,40 @@ public class MenuManager {
       System.out.println(Emoji.TWO.getEmoji() + " " + MenuText.BACK_TO_MENU.getMenuText());
       System.out.println(
           Colors.YELLOW.getColor() + MenuText.CHOICE.getMenuText() + Colors.RESET.getColor());
-      int choice = scanner.nextInt();
-      scanner.nextLine();
 
-      switch (choice) {
-        case 1:
-          String randomFact = interestingFacts.getRandomFact();
-          System.out.println(Colors.YELLOW.getColor() + Separator.LONG_SIMPLE_LINE.getSeparator()
-              + Colors.RESET.getColor());
-          System.out.println(randomFact);
-          System.out.println(Colors.YELLOW.getColor() + Separator.LONG_SIMPLE_LINE.getSeparator()
-              + Colors.RESET.getColor());
-          break;
-        case 2:
-          System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
-              + Colors.RESET.getColor());
-          return;
-        default:
-          System.out.println(
-              Emoji.WRONG.getEmoji() + " " + Colors.RED.getColor() + Message.ERROR.getMessage()
-                  + Colors.RESET.getColor());
-          break;
+      if (scanner.hasNextInt()) {
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+          case 1:
+            String randomFact = interestingFacts.getRandomFact();
+            System.out.println(Colors.YELLOW.getColor() + Separator.LONG_SIMPLE_LINE.getSeparator()
+                + Colors.RESET.getColor());
+            System.out.println(randomFact);
+            System.out.println(Colors.YELLOW.getColor() + Separator.LONG_SIMPLE_LINE.getSeparator()
+                + Colors.RESET.getColor());
+            break;
+          case 2:
+            System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
+                + Colors.RESET.getColor());
+            return;
+          default:
+            System.out.println(
+                Emoji.WRONG.getEmoji() + " " + Colors.RED.getColor() + Message.ERROR.getMessage()
+                    + Colors.RESET.getColor());
+            break;
+        }
+      } else {
+        System.out.println(
+            Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Введи цифру 1 или 2"
+                + Colors.RESET.getColor());
+        scanner.next();
       }
     }
   }
 
   private void runVocabularyTrainingSubMenu() {
-    scanner = new Scanner(System.in);
     System.out.println(
         Colors.YELLOW.getColor() + Separator.UPPER_LINE.getSeparator() + Colors.RESET.getColor());
     System.out.println(Emoji.GO_AHEAD.getEmoji() + " Давай проверим твои знания слов из словарика!");
@@ -188,25 +208,33 @@ public class MenuManager {
       System.out.println(Emoji.THREE.getEmoji() + " " + MenuText.BACK_TO_MENU.getMenuText());
       System.out.println(
           Colors.YELLOW.getColor() + MenuText.CHOICE.getMenuText() + Colors.RESET.getColor());
-      int choice = scanner.nextInt();
-      scanner.nextLine();
 
-      switch (choice) {
-        case 1:
-          vocabularyTraining.runSpanishToRussianTraining();
-          break;
-        case 2:
-          vocabularyTraining.runRussianToSpanishTraining();
-          break;
-        case 3:
-          System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
-              + Colors.RESET.getColor());
-          return;
-        default:
-          System.out.println(
-              Emoji.WRONG.getEmoji() + " " + Colors.RED.getColor() + Message.ERROR.getMessage()
-                  + Colors.RESET.getColor());
-          break;
+      if (scanner.hasNextInt()) {
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+          case 1:
+            vocabularyTraining.runSpanishToRussianTraining();
+            break;
+          case 2:
+            vocabularyTraining.runRussianToSpanishTraining();
+            break;
+          case 3:
+            System.out.println(Colors.YELLOW.getColor() + Separator.SIMPLE_LINE.getSeparator()
+                + Colors.RESET.getColor());
+            return;
+          default:
+            System.out.println(
+                Emoji.WRONG.getEmoji() + " " + Colors.RED.getColor() + Message.ERROR.getMessage()
+                    + Colors.RESET.getColor());
+            break;
+        }
+      } else {
+        System.out.println(
+            Emoji.WRONG.getEmoji() + Colors.RED.getColor() + " Введи цифру 1, 2 или 3"
+                + Colors.RESET.getColor());
+        scanner.next();
       }
     }
   }
